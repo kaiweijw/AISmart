8ca3099bf68ff5d9139961d8c8d5cf48a3d50834
Stanik-13 <stanik.xu@aetherlink.network>
2024-12-04 14:46:52 +0800

    feat:add elasticsearch

--- src/AISmart.Silo/SiloModule.cs
+++ src/AISmart.Silo/SiloModule.cs
@@ -2,6 +2,7 @@ using AElf.OpenTelemetry;
 using AISmart.Domain.Grains;
 using Microsoft.Extensions.DependencyInjection;
 using AISmart.Application.Grains;
+using Nest;
 using Serilog;
 using Volo.Abp.AspNetCore.Serilog;
 using Volo.Abp.Autofac;
@@ -21,10 +22,26 @@ public class SiloModule : AbpModule, IDomainGrainsModule, IApplicationGrainsModu
         Configure<AbpAutoMapperOptions>(options => { options.AddMaps<SiloModule>(); });
         context.Services.AddHostedService<AISmartHostedService>();
         var configuration = context.Services.GetConfiguration();
-        //add dependencies here
+        // add elasticSearch
+        context = AddElasticSearch(context);
+        
         context.Services.AddSerilog(loggerConfiguration => {},
             true, writeToProviders: true);
-
+        
+        
         context.Services.AddHttpClient();
+    }
+
+    private ServiceConfigurationContext AddElasticSearch(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        context.Services.AddSingleton<IElasticClient>(provider =>
+        {
+            var settings =new ConnectionSettings(new Uri(configuration["Elasticsearch:Uri"]))
+                .DefaultIndex("grain-events");
+            return new ElasticClient(settings);
+        });
+
+        return context;
     }
 }
