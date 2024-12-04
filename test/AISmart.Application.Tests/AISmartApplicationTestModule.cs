d38bc4e9b84903407a1dab368cc4c14a3930b930
jason20240511 <jason.zhang@aelf.io>
2024-12-04 20:24:21 +0800

    add appsettings.json config

--- test/AISmart.Application.Tests/AISmartApplicationTestModule.cs
+++ test/AISmart.Application.Tests/AISmartApplicationTestModule.cs
@@ -18,6 +18,7 @@ public class AISmartApplicationTestModule : AbpModule
         base.ConfigureServices(context);
         context.Services.AddSingleton<IDaprProvider , MockDaprProvider>();
         Configure<AbpAutoMapperOptions>(options => { options.AddMaps<AISmartApplicationModule>(); });
-           
+        var configuration = context.Services.GetConfiguration();
+        Configure<ChatConfigOptions>(configuration.GetSection("Chat"));   
     }
 }
--- test/AISmart.Application.Tests/Mock/MockDaprProvider.cs
+++ test/AISmart.Application.Tests/Mock/MockDaprProvider.cs
@@ -1,15 +1,17 @@
 using System;
+using System.ClientModel;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AISmart.AgentTask;
 using AISmart.Application.Grains.Event;
 using AISmart.Dapr;
 using AISmart.Domain.Grains.Event;
-using AutoGen.BasicSample;
 using AutoGen.Core;
 using AutoGen.OpenAI;
 using AutoGen.OpenAI.Extension;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OpenAI;
 using Orleans;
 
 namespace AISmart.Mock;
@@ -20,9 +22,12 @@ public class MockDaprProvider :  IDaprProvider
     private readonly string _twitterTopic = "twitter";
     private readonly string _telegramTopic = "Telegram";
     private readonly string _gptTopic = "GPT";
+    private readonly ChatConfigOptions _chatConfigOptions;
+
     public MockDaprProvider(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
+        _chatConfigOptions = serviceProvider.GetRequiredService<IOptionsSnapshot<ChatConfigOptions>>().Value;
     }
     public async Task PublishEventAsync<T>(string pubsubName, string topicName, T message)
     {
@@ -41,9 +46,8 @@ public class MockDaprProvider :  IDaprProvider
             }else if (topicName == _gptTopic)
             {
                 var task =  await _serviceProvider.GetRequiredService<AgentTaskService>().GetAgentTaskDetailAsync(agentEvent.TaskId);
-                var gpt4oMini = LLMConfiguration.GetOpenAIGPT4o_mini();
                 var assistantAgent = new OpenAIChatAgent(
-                        chatClient: gpt4oMini,
+                        chatClient: new OpenAIClient(_chatConfigOptions.APIKey).GetChatClient(_chatConfigOptions.Model),
                         name: "assistant",
                         systemMessage: "You convert what user said to all uppercase.")
                     .RegisterMessageConnector()
