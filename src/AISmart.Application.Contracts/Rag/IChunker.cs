7335ab3d159fca52974e6f6b64d5c27f15ed6efc
jim.dong <jim.dong@aelf.io>
2024-12-11 20:40:09 +0800

    Chunker finished

--- src/AISmart.Application.Contracts/Rag/IChunker.cs
+++ src/AISmart.Application.Contracts/Rag/IChunker.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AISmart.Rag;
 
 public interface IChunker
 {
-    IEnumerable<string> Chunk(string text);
+    public Task<List<string>> Chunk(string text, int chunkSize);
 }
