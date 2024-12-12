425dc0c21ed584779242a6df48fc4089058bdf5e
louis.li <louis.li@aelf.io>
2024-12-12 17:06:56 +0800

    feat: update host config

--- src/AISmart.HttpApi.Host/AISmartHttpApiHostModule.cs
+++ src/AISmart.HttpApi.Host/AISmartHttpApiHostModule.cs
@@ -66,7 +66,7 @@ public class AISmartHttpApiHostModule : AIApplicationGrainsModule, IDomainGrains
 
         ConfigureAuthentication(context);
         ConfigureBundles();
-        ConfigureUrls(configuration);
+       // ConfigureUrls(configuration);
         ConfigureConventionalControllers();
         ConfigureVirtualFileSystem(context);
         ConfigureCors(context, configuration);
@@ -209,8 +209,7 @@ public class AISmartHttpApiHostModule : AIApplicationGrainsModule, IDomainGrains
 
         app.UseUnitOfWork();
         app.UseDynamicClaims();
-        app.UseAuthorization();
-
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         app.UseSwagger();
         app.UseAbpSwaggerUI(c =>
         {
