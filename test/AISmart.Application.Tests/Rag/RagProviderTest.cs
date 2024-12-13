858bee19537433d8a153d18880a4fa1d97dd744a
house1313 <house@schrodingernft.ai>
2024-12-13 09:55:52 +0800

    feat: update test case

--- test/AISmart.Application.Tests/Rag/RagProviderTest.cs
+++ test/AISmart.Application.Tests/Rag/RagProviderTest.cs
@@ -30,20 +30,22 @@ public class RagProviderTest : AISmartApplicationTestBase
 
         var texts = new List<string>
         {
-            "RAG功能强大。",
-            "在欧冠第6轮比赛中",
-            "RAG包括chunk, embedding以及vectordb。",
-            "姆巴佩破门",
-            "RAG通过引入外部文档来缓解LLM的固有限制",
-            "帮助皇马客场3-2击败亚特兰大"
+            "Retrieval-Augmented Generation (RAG) is a technique that enhances language model generation by incorporating external knowledge",
+            "Yet while many critics are dismayed at the prospect, few should be surprised given the influence the kingdom's unprecedented investment in sport has secured.",
+            "With RAG, the LLM is able to leverage knowledge and information that is not necessarily in its weights by providing it access to external knowledge sources such as databases",
+            "Fifa has defended a fast-tracked process that many argue was lacking in transparency and accountability",
+            "It leverages a retriever to find relevant contexts to condition the LLM, in this way, RAG is able to augment the knowledge-base of an LLM with relevant documents",
+            "So is the tournament being used to help transform Saudi Arabia's reputation, or can it be a catalyst for social reform? And what does this tell us about Fifa and football more widely?"
         };
         
         await ragProvider.BatchStoreTextsAsync(texts);
 
         var keyword = "RAG";
-        var queryEmbedding = await embeddingProvider.GetEmbeddingAsync(keyword+"是什么？");
-        var retrievedChunks = await vectorDatabase.RetrieveAsync(queryEmbedding,3);
-        foreach (var chunk in retrievedChunks)
+        var question = "what is " + keyword;
+        var queryEmbedding = await embeddingProvider.GetEmbeddingAsync(question);
+        var answer = await vectorDatabase.RetrieveAsync(queryEmbedding,3);
+        
+        foreach (var chunk in answer)
         {
             Assert.Contains(keyword, chunk);
         }
