e4487cb6784e112386939d19cf7295f8ff464ce5
eanzhao <yiqi.zhao@aelf.io>
2024-12-13 17:13:06 +0800

    test: improve group test.

--- test/AISmart.Agents.Tests/AgentsTests.cs
+++ test/AISmart.Agents.Tests/AgentsTests.cs
@@ -1,5 +1,8 @@
 using AISmart.Agents.X.Events;
+using AISmart.Application.Grains.Agents.Developer;
 using AISmart.Application.Grains.Agents.Group;
+using AISmart.Application.Grains.Agents.Investment;
+using AISmart.Application.Grains.Agents.MarketLeader;
 using AISmart.Application.Grains.Agents.Sender;
 using AISmart.Application.Grains.Agents.X;
 using Orleans.TestKit;
@@ -15,8 +18,15 @@ public class PipelineTest : TestKitBase
         var groupAgent = await Silo.CreateGrainAsync<GroupAgent>(Guid.NewGuid());
         var publishingAgent = await Silo.CreateGrainAsync<PublishingAgent>(Guid.NewGuid());
         var xAgent = await Silo.CreateGrainAsync<XAgent>(Guid.NewGuid());
+        var marketLeaderAgent = await Silo.CreateGrainAsync<MarketLeaderAgent>(Guid.NewGuid());
+        var developerAgent = await Silo.CreateGrainAsync<DeveloperAgent>(Guid.NewGuid());
+        var investmentAgent = await Silo.CreateGrainAsync<InvestmentAgent>(Guid.NewGuid());
 
         await groupAgent.Register(xAgent);
+        await groupAgent.Register(marketLeaderAgent);
+        await groupAgent.Register(developerAgent);
+        await groupAgent.Register(investmentAgent);
+
         await publishingAgent.PublishTo(groupAgent);
 
         var xThreadCreatedEvent = new XThreadCreatedEvent
@@ -29,5 +39,11 @@ public class PipelineTest : TestKitBase
 
         var xAgentState = await xAgent.GetStateAsync();
         xAgentState.ThreadIds.Count.ShouldBe(1);
+        
+        var investmentAgentState = await investmentAgent.GetStateAsync();
+        investmentAgentState.Content.Count.ShouldBe(1);
+        
+        var developerAgentState = await developerAgent.GetStateAsync();
+        developerAgentState.Content.Count.ShouldBe(1);
     }
 }
