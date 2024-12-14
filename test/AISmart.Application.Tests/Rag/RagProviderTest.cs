367b23bcb0f3cb1bcda94ff1d9c9c68b3c1e2612
house1313 <house@schrodingernft.ai>
2024-12-14 21:43:30 +0800

    feat: add semantic unittest

--- test/AISmart.Application.Tests/Rag/RagProviderTest.cs
+++ test/AISmart.Application.Tests/Rag/RagProviderTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AISmart.Options;
 using AISmart.Provider;
@@ -42,40 +43,45 @@ public class RagProviderTest : AISmartApplicationTestBase
         Assert.Contains(keyword, answer);
     }
     
-    // [Fact]
-    // public async Task StoreBatch1TestAsync_Test()
-    // {
-    //     var config = GetRequiredService<IOptionsMonitor<RagOptions>>();
-    //     IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
-    //     kernelBuilder.AddOpenAITextEmbeddingGeneration("text-embedding-ada-002", config.CurrentValue.APIKey);
-    //     var kernel = kernelBuilder.Build();
-    //     
-    //     var embeddingGenerator = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
-    //     
-    //     var memoryStore = new QdrantMemoryStore("http://localhost:6333", 1536);
-    //     SemanticTextMemory textMemory = new(memoryStore, embeddingGenerator);
-    //     
-    //     var texts = new List<string>
-    //     {
-    //         "Retrieval-Augmented Generation (RAG) is a technique that enhances language model generation by incorporating external knowledge",
-    //         "Yet while many critics are dismayed at the prospect, few should be surprised given the influence the kingdom's unprecedented investment in sport has secured.",
-    //         "With RAG, the LLM is able to leverage knowledge and information that is not necessarily in its weights by providing it access to external knowledge sources such as databases",
-    //         "Fifa has defended a fast-tracked process that many argue was lacking in transparency and accountability",
-    //         "It leverages a retriever to find relevant contexts to condition the LLM, in this way, RAG is able to augment the knowledge-base of an LLM with relevant documents",
-    //         "So is the tournament being used to help transform Saudi Arabia's reputation, or can it be a catalyst for social reform? And what does this tell us about Fifa and football more widely?"
-    //     };
-    //
-    //     var collectionName = "test_collection7";
-    //     foreach (var km in texts)
-    //     {
-    //         await textMemory.SaveInformationAsync(
-    //             collection: collectionName,
-    //             text: km,
-    //             id: Guid.NewGuid().ToString());
-    //     }
-    //     
-    //     var chunker = new OpenAIChunker();
-    //     
-    // }
+    [Fact]
+    public async Task StoreBatch1TestAsync_Test()
+    {
+        var config = GetRequiredService<IOptionsMonitor<RagOptions>>();
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+        kernelBuilder.AddOpenAITextEmbeddingGeneration("text-embedding-ada-002", config.CurrentValue.APIKey);
+        var kernel = kernelBuilder.Build();
+        
+        var embeddingGenerator = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
+        
+        var memoryStore = new QdrantMemoryStore(config.CurrentValue.QdrantUrl, 1536);
+        SemanticTextMemory textMemory = new(memoryStore, embeddingGenerator);
+        
+        var texts = new List<string>
+        {
+            "Retrieval-Augmented Generation (RAG) is a technique that enhances language model generation by incorporating external knowledge",
+            "Yet while many critics are dismayed at the prospect, few should be surprised given the influence the kingdom's unprecedented investment in sport has secured.",
+            "With RAG, the LLM is able to leverage knowledge and information that is not necessarily in its weights by providing it access to external knowledge sources such as databases",
+            "Fifa has defended a fast-tracked process that many argue was lacking in transparency and accountability",
+            "It leverages a retriever to find relevant contexts to condition the LLM, in this way, RAG is able to augment the knowledge-base of an LLM with relevant documents",
+            "So is the tournament being used to help transform Saudi Arabia's reputation, or can it be a catalyst for social reform? And what does this tell us about Fifa and football more widely?"
+        };
+    
+        var collectionName = config.CurrentValue.CollectionName;
+        foreach (var km in texts)
+        {
+            await textMemory.SaveInformationAsync(
+                collection: collectionName,
+                text: km,
+                id: Guid.NewGuid().ToString());
+        }
+        
+        var keyword = "RAG";
+        var result = await textMemory.SearchAsync(collection: collectionName, query: keyword, limit: 2, minRelevanceScore: 0.5).ToListAsync();
+        foreach (var item in result)
+        {
+            Assert.Contains(keyword, item.Metadata.Text.ToString());
+        }
+        
+    }
     
 }
