84bf88b4180071794e40f7b6ee9a61285ae1adc5
jason20240511 <jason.zhang@aelf.io>
2024-12-14 11:16:05 +0800

    update MongoStorage

--- test/AISmart.Application.Tests/Mongo/Agent/SenderAgent.cs
+++ test/AISmart.Application.Tests/Mongo/Agent/SenderAgent.cs
@@ -2,6 +2,7 @@
 
 using System.Threading.Tasks;
 using AISmart.Agents.X.Events;
+using Google.Cloud.AIPlatform.V1;
 using Orleans;
 using Orleans.Providers;
 
@@ -17,6 +18,11 @@ public interface ISenderAgent:IGrainWithGuidKey
 public class SenderAgentState
 {
     [Id(0)]  public string Content { get; set; }
+
+    public void Apply(XThreadCreatedEvent xThreadCreatedEvent)
+    {
+        
+    }
 }
 
 
@@ -33,7 +39,6 @@ public class SenderAgent : MongoStorage<SenderAgentState, XThreadCreatedEvent>, 
 
     public Task PublishEventAsync<T>(T @event)
     {
-        State.Content = (@event as XThreadCreatedEvent).Content+" commit";
         RaiseEvent((@event as XThreadCreatedEvent));
         base.ConfirmEvents();
         return Task.CompletedTask;
