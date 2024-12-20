d88ce69e627216bad3f453f04bb561ea5dc402c0
suerlych <19871208@lhcyf>
2024-12-20 15:53:16 +0800

    ad AISmart.Cqrs.Tests

---
+++ test/AISmart.Cqrs.Tests/Cqrs/CqrsProviderTest.cs
@@ -0,0 +1,59 @@
+using AISmart.Agent;
+using AISmart.Agent.Events;
+using AISmart.CQRS;
+using AISmart.CQRS.Dto;
+using AISmart.CQRS.Handler;
+using AISmart.CQRS.Provider;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AISmart.GAgent;
+
+public class CqrsProviderTest : AISmartApplicationTestBase
+{
+    private readonly IClusterClient _clusterClient;
+    private readonly ICQRSProvider _cqrsProvider;
+    private readonly Mock<IIndexingService> _mockIndexingService;
+    private const string ChainId = "AELF";
+    private const string SenderName = "Test";
+    private const string Address = "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE";
+    private const string IndexName = "aelfagentgstateindex";
+    private const string IndexId = "1";
+
+    public CqrsProviderTest(ITestOutputHelper output)
+    {
+        _clusterClient = GetRequiredService<IClusterClient>();
+        _mockIndexingService = new Mock<IIndexingService>();
+        _mockIndexingService.Setup(service => service.SaveOrUpdateIndexAsync(It.IsAny<string>(), It.IsAny<BaseStateIndex>()))
+            .Returns(Task.CompletedTask);
+        _mockIndexingService.Setup(b => b.QueryIndexAsync(It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync((string id, string indexName) => new BaseStateIndex { Id = IndexId.ToString(), Ctime = DateTime.Now, State = Address});
+
+        var services = new ServiceCollection();
+        services.AddSingleton<IIndexingService>(_mockIndexingService.Object); 
+        services.AddMediatR(typeof(SaveStateCommandHandler).Assembly);
+        services.AddMediatR(typeof(GetStateQueryHandler).Assembly);
+        services.AddMediatR(typeof(SendEventCommandHandler).Assembly);
+        services.AddSingleton<ICQRSProvider,CQRSProvider>();
+        services.AddSingleton<IGrainFactory>(_clusterClient);
+        var serviceProvider = services.BuildServiceProvider();
+        _cqrsProvider = serviceProvider.GetRequiredService<ICQRSProvider>();
+    }
+    
+    [Fact]
+    public async Task SendEventCommandTest()
+    {
+        var createTransactionEvent = new CreateTransactionEvent()
+        {
+            ChainId = ChainId,
+            SenderName = SenderName,
+            ContractAddress = Address,
+            MethodName = "Transfer",
+        };
+        await _cqrsProvider.SendEventCommandAsync(createTransactionEvent);
+    }
+}
